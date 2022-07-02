c11e426b2ec1dfef754eb5a9cd563645fa7b465d
ADITYA-HEBBAR <84819735+Aditya21Hebbar@users.noreply.github.com>
2022-07-02 18:11:23 +0000

    step 4

--- scraper.py
+++ scraper.py
@@ -6,6 +6,9 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 import time
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support import expected_conditions as EC
 
 url = 'https://food.grab.com/sg/en/restaurants'
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
@@ -16,6 +19,7 @@ def get_driver():
   chrome_options.add_argument('--headers')
   chrome_options.add_argument('--disable-dev-shm-usage')
   driver = webdriver.Chrome(options=chrome_options)
+  driver.maximize_window()
   return driver 
   
 
@@ -39,9 +43,15 @@ if __name__ == "__main__":
   driver = get_driver()
   # print('fetching the top list restaurants')
   driver.get(url)
-  # while True:
-  #   try:
-  #     print("hello")
+  WebDriverWait(driver, 20).until(EC.visibility_of_element_located((By.XPATH, "/html/body/div[1]/div[2]/div[5]/div[4]/div/div/div[4]/div/button")))
+  while True:
+    try:
+      time.sleep(2)
+      WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[1]/div[2]/div[5]/div[4]/div/div/div[4]/div/button"))).click()
+      print("MORE button clicked")
+      time.sleep(2)
+    except TimeoutException:
+        break
   #     loadMoreButton = driver.find_element(By.CSS_SELECTOR,'#page-content > div:nth-child(4) > div > div > div:nth-child(5) > div > button')
   #     time.sleep(2)
   #     loadMoreButton.click()
@@ -52,6 +62,7 @@ if __name__ == "__main__":
   #     print ("Complete")
   #     time.sleep(10)
   #     driver.quit()
+  print('fetching page...!')
   r= driver.page_source
   soup = BeautifulSoup(r,'html.parser')
   data = json.loads(soup.find('script', id='__NEXT_DATA__').text)
