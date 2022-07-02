c8fa01474c9b6138adb38a825345fd6eb341495a
ADITYA-HEBBAR <84819735+Aditya21Hebbar@users.noreply.github.com>
2022-07-02 12:06:35 +0000

    simple code that works without more data

--- scraper.py
+++ scraper.py
@@ -1,17 +1,31 @@
 import requests
 import json
 from bs4 import BeautifulSoup
+import pandas as pd
 
 url = 'https://food.grab.com/sg/en/restaurants'
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
 
+
+def 
 r = requests.get(url,headers = headers)
 soup = BeautifulSoup(r.content,'html.parser')
-#script = soup.find_all('script')[0]
-
 
 data = json.loads(soup.find('script', id='__NEXT_DATA__').text)
-# print(data['props']['initialReduxState']['pageRestaurantsV2']['entities']['restaurantList'])
+
 restaurant_list = data['props']['initialReduxState']['pageRestaurantsV2']['entities']['restaurantList']
-restaurants = list(restaurant_list.items())
-print(restaurants)
+
+position_data=[]
+for restaurant in restaurant_list:
+    restaurant_data = restaurant_list[restaurant]
+    latitude  = restaurant_data['latitude']
+    longitude = restaurant_data['longitude']
+    geo_data = {
+        'latitude': latitude,
+        'longitude': longitude
+    }
+    position_data.append(geo_data)
+position_data
+
+result = pd.DataFrame(position_data)
+result
