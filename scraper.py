3df85266a8a4419177c6e178bc281f0d3dc8e1eb
ADITYA-HEBBAR <84819735+Aditya21Hebbar@users.noreply.github.com>
2022-07-02 06:20:54 +0000

    scfl code

--- scraper.py
+++ scraper.py
@@ -5,6 +5,10 @@ from bs4 import BeautifulSoup
 url = 'https://food.grab.com/sg/en/restaurants'
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
 
-r = requests.get(url,headers=headers)
+r = requests.get(url,headers = headers)
+soup = BeautifulSoup(r.content,'html.parser')
+script = soup.find_all('script')[0]
 
-print(r.status_code)
+
+data = json.loads(soup.find('script', id='__NEXT_DATA__').text)
+print(data)
